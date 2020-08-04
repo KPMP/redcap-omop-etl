673f1d703f1fc6def28d4ba6d0306932a0773d17
Jonas Carson <jcarson@uw.edu>
2020-08-04 09:14:54 -0700

    -- minor revisions to take an existing copy of the csv file and add to it and to pull down all fields

--- dictionary_extraction.py
+++ dictionary_extraction.py
@@ -1,17 +1,12 @@
 import requests
 import re
+import os
 import sys
 import datetime
 import pandas as pd
 import numpy as np
 
 url = 'https://redcap.kpmp.org/api/'
-
-field_table = pd.read_csv(sys.argv[1])
-token = token_printer(sys.argv[2])
-form_field_dict = field_table.groupby('Form').Field.apply(list).to_dict()
-form_event_dict = field_table.groupby('Form').Event.apply(lambda x: list(set(x))[0]).to_dict()
-
 
 def token_printer(auth_file):
     auth_regex = re.compile(r'(?<=TOKEN=)[0-9A-Za-z]+')
@@ -21,34 +16,54 @@ def token_printer(auth_file):
     return auth_regex.search(token_line).group(0)
 
 
-def dict_extract(form_event_map):
+def dict_extract(existing_df):
     init_columns = ['field_name', 'form_name', 'section_header', 'field_type',
                     'field_label', 'select_choices_or_calculations', 'field_note',
                     'text_validation_type_or_show_slider_number', 'text_validation_min',
                     'text_validation_max', 'identifier', 'branching_logic',
                     'required_field', 'custom_alignment', 'question_number',
                     'matrix_group_name', 'matrix_ranking', 'field_annotation']
-    keep_columns = ['field_name', 'form_name', 'field_type', 'select_choices_or_calculations',
+    keep_columns = ['form_name', 'field_name', 'field_type', 'select_choices_or_calculations',
                     'field_label', 'text_validation_type_or_show_slider_number']
-    addition_df = pd.DataFrame(columns=['status', 'exclude_reason', 'notes', 'ontology_term', 'restrict_to_event_list'])
+    addition_columns = ['status', 'exclude_reason', 'notes', 'ontology_term', 'restrict_to_event_list']
+    addition_df = pd.DataFrame(columns=addition_columns)
+
+
+    column_order = ['form_name', 'field_name', 'status', 'exclude_reason', 'notes', 'field_type', 'select_choices_or_calculations',
+                    'field_label', 'text_validation_type_or_show_slider_number','ontology_term', 'restrict_to_event_list']
+
     extraction_df = pd.DataFrame(columns=init_columns)
-    for i, k in enumerate(form_event_map.keys()):
-        data_event = \
-            {
-                'token': token,
-                'content': 'metadata',
-                'format': 'json',
-                'returnFormat': 'json',
-                f'forms[{i}]': k
-            }
-        response = requests.post(url, data=data_event)
-        # header = response.headers
-        df = pd.json_normalize(response.json())
-        extraction_df = pd.concat([extraction_df, df])
+    
+    data_event = \
+        {
+            'token': token,
+            'content': 'metadata',
+            'format': 'json',
+            'returnFormat': 'json',
+        }
+    response = requests.post(url, data=data_event)
+    # header = response.headers
+    df = pd.json_normalize(response.json())
+    extraction_df = pd.concat([extraction_df, df])
     extraction_df = extraction_df.drop(columns=list(set(init_columns) - set(keep_columns)))
+    
     extraction_df = pd.concat([extraction_df, addition_df], axis=1)
+    extraction_df = extraction_df[column_order]
+
+    # copy in data from existing df that is in the addition column list (Status, etc)
+    if existing_df:
+        extraction_df[addition_columns] = existing_df[existing_df['field_name'] == extraction_df['field_name']][addition_columns]
+    
     return extraction_df
 
 
-extraction_dictionary = dict_extract(form_event_dict)
+token = token_printer(sys.argv[1])
+existing_csv = sys.argv[2]
+existing_df = None
+try:
+    existing_df = pd.read_csv(existing_csv)
+except pd.io.common.EmptyDataError:
+    print("Empty existing csv passed as arg 2. Assuming we have no existing data to transfer")
+
+extraction_dictionary = dict_extract(existing_df)
 extraction_dictionary.to_csv(sys.argv[3], index=False)
